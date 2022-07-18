e127584c77a351d207b37b39fb5173f61db50dd8
Bored ᓚᘏᗢ <79646021+lmBored@users.noreply.github.com>
2022-07-18 23:33:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,15 +4,15 @@ include("auth.php"); ?>
 <html>
 <head>
 <meta charset="utf-8">
-<title>WEB CƠ BẢN</title>
+<title>Web by lmbored</title>
 <link rel="stylesheet" href="css/style.css" />
 </head>
 <body>
 <div class="form">
-<p>Xin chào <?php echo $_SESSION['username']; ?>!</p>
-<p>Đây là trang chủ</p>
-<p><a href="dashboard.php">Bảng điều khiển</a></p>
-<a href="logout.php">Đăng xuất</a>
+<p>Hi <?php echo $_SESSION['username']; ?>!</p>
+<p>Welcome to my web</p>
+<p><a href="dashboard.php">Dashboard</a></p>
+<a href="logout.php">Logout</a>
 </div>
 </body>
 </html>
