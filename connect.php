ec43a983497654ee07c429f32697a14dfd64c0ee
Bored ᓚᘏᗢ <79646021+lmBored@users.noreply.github.com>
2022-07-18 23:31:46 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,6 +2,6 @@
 $con = mysqli_connect("localhost","root","","data");
 if (mysqli_connect_errno())
   {
-  echo "Không thể kết nối đến MySQL: " . mysqli_connect_error();
+  echo "Can't connect to MySQL: " . mysqli_connect_error();
   }
 ?>
